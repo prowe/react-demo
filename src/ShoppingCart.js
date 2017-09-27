f86611770dc057f10e09fc124113f9772f0f714f
Paul <paul.w.rowe@gmail.com>
2017-09-26 20:58:51 -0500

    cleaned up shopping cart

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -10,12 +10,10 @@ export default class ShoppingCart extends Component {
                 lineItems: []
             }
         };
-
-        this.createListItem = this.createListItem.bind(this);
     }
     
     componentDidMount() {
-        fetch(`http://localhost:8080/shopping-carts/${window.shoppingCartId}`)
+        fetch(`http://localhost:8080/shopping-carts/123`)
             .then(res => res.json())
             .then(cart => Promise.all(cart.lineItems.map(this.loadAndPopulateProduct))
                 .then(() => this.setState({cart: cart}))
@@ -29,34 +27,41 @@ export default class ShoppingCart extends Component {
     }
 
     render() {
-        let cart = this.state.cart;
-        return <ul className="mdc-list mdc-list--avatar-list">
-            {cart.lineItems.map(this.createListItem)}
-        </ul>;
+        let lineItemElements = this.state.cart.lineItems
+            .map(li => <CartItem lineItem={li} key={li.lineItemId}/>);
+
+        return <ul className="mdc-list mdc-list--avatar-list">{lineItemElements}</ul>;
+    }
+}
+
+class CartItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lineItem: props.lineItem
+        };
     }
 
-    createListItem(item) {
-        return <li className="mdc-list-item" key={item.lineItemId} >
-            <img src={item.product.imageURL} className="mdc-list-item__start-detail" />
-            <span className="mdc-list-item__text">
-                {item.product.name}
-            </span>
-            <a className="mdc-list-item__end-detail material-icons" 
-                href="#" 
-                onClick={e => this.removeLineItemFromCart(e, item)}>remove_shopping_cart</a>
-        </li>;
-    }
-
-    removeLineItemFromCart(e, lineItem) {
+    removeFromCart = (e) => {
         e.preventDefault();
-
-        fetch(`http://localhost:8080/shopping-carts/${window.shoppingCartId}/line-items/${lineItem.lineItemId}`, {
-                method: "DELETE"
+        
+        let lineItem = this.state.lineItem;
+        fetch(`http://localhost:8080/shopping-carts/123/line-items/${lineItem.lineItemId}`, {
+            method: "DELETE"
         })
-        .then(() => {
-            let cart = this.state.cart;
-            cart.lineItems = cart.lineItems.filter(li => li != lineItem);
-            this.setState({cart: cart});
-        });
+        .then(() => this.setState({lineItem: null}));
+    }
+
+    render() {
+        let item = this.state.lineItem;
+        return !item ? null : 
+            <li className="mdc-list-item">
+                <img src={item.product.imageURL} className="mdc-list-item__start-detail" />
+                <span className="mdc-list-item__text">
+                    {item.product.name}
+                </span>
+                <a className="mdc-list-item__end-detail material-icons"  
+                    onClick={this.removeFromCart}>remove_shopping_cart</a>
+            </li>;
     }
 }
