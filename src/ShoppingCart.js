773f68e82b610c2da3916ae5b7213927ed3aefcd
prowe <paul.w.rowe@gmail.com>
2017-10-16 07:14:49 -0500

    creating shell

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -12,56 +12,24 @@ export default class ShoppingCart extends Component {
     }
 
     componentDidMount(){
-        fetch('http://localhost:5000/shopping-carts/1')
-            .then(res => res.json())
-            .then(cart => Promise.all(cart.lineItems.map(li => 
-                fetch(`http://localhost:8080/products/${li.productId}`)
-                    .then(res => res.json())
-                    .then(product => li.product = product)))
-                .then(() => this.setState({cart}))
-            );
-    }
-
-    lineItemRemoved = lineItem => {
-        let cart = this.state.cart;
-        this.setState({
-            cart: {
-                ...cart,
-                lineItems: cart.lineItems.filter(li => li != lineItem)
-            }
-        })
+        
     }
 
     render() {
         return <ul>
-            {this.state.cart.lineItems.map(li => <CartItem 
-                lineItem={li} 
-                lineItemRemoved={this.lineItemRemoved} />)}
+            {this.state.cart.lineItems.map(li => <CartItem lineItem={li} />)}
         </ul>;
     }
 }
 
 class CartItem extends Component {
-    removeFromCart = (e) => {
-        e.preventDefault();
-
-        let lineItem = this.props.lineItem;
-        fetch(`http://localhost:5000/shopping-carts/1/line-items/${lineItem.id}`, {
-            method: 'DELETE'
-        })
-        .then(() => {
-            if(this.props.lineItemRemoved) {
-                this.props.lineItemRemoved(lineItem);
-            }
-        })
-    }
 
     render() {
         let item = this.props.lineItem;
         return <li>
             <img src={item.product.imageURL} height='50px' />
             {item.product.name} ({item.quantity} @ ${item.product.price})
-            <a href="#" onClick={this.removeFromCart}>Remove</a>
+            /* <a href="#" onClick={this.removeFromCart}>Remove</a> */
         </li>
     }
 }
